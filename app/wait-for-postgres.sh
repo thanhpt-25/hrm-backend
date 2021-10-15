5be5090a1905452f2b0f99539b29545c40aaef00
Phạm Tiến Thành <thanh.pham@miichisoft.com>
2021-10-15 11:57:21 +0700

    add wait for postgress to ensure that api only run after database finished its setup process

---
+++ app/wait-for-postgres.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# wait-for-postgres.sh
+until PGPASSWORD=$POSTGRES_PASSWORD PGUSER=$POSTGRES_USER PGHOST=$POSTGRES_DB_HOST PGDATABASE=$POSTGRES_DB_PREFIX"_"$POSTGRES_DB_NAME psql -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up"
+exec "$@"
